3c5ddadf8897e55b5453ca8d5d9b3775fbeac398
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-10-20 12:49:13 -0700

    Debugging API mounting function

--- app/script.js
+++ app/script.js
@@ -1,13 +1,13 @@
 // Set the original zoom level of the example image viewed
 var zoom_level = 17;
 
-// zoomUpdate() will alter our image zoom value from the min 17
-function zoomUpdate(value) {
+// updateZoom() will alter our image zoom value from the min 17
+function updateZoom(value) {
   zoom_level = parseInt(document.querySelector("#zoom").value);
 
   if(document.querySelector("#zoom").value >= 20) {
     document.querySelector("#msg").innerHTML = "A higher zoom level means long loading times.";
-    document.querySelector("#msg").style = "color:red";
+    document.querySelector("#msg").style = "color:red;";
   }
   else if (document.querySelector("#zoom").value < 20) {
     document.querySelector("#msg").innerHTML = "";
@@ -25,23 +25,23 @@ function ddApiMounted() {
 };
 
 // notifyError() will let us know if there was any error for zoomUpdate()
-funtion notifyError(err, msg) {
+function notifyError(e, msg) {
   document.querySelector("#msg").innerHTML = msg + "Console check for errors.";
-  document.querySelector("#msg").style = "color:red";
-  throw new Error(err);
+  document.querySelector("#msg").style = "color:red;";
+  throw new Error(e);
 };
 
-// generateListener() will l
+// generateListener() gets plans
 function generateListener() {
   document.querySelector("#msg").innerHTML = "Generating...";
   document.querySelector("#msg").style = "";
 
-  getCurrentlyViewedPlan()                                       .catch(err => reportError(err,"Error getting Plan."))
-    .then(plan         => getTileDataFromPlan(plan)              .catch(err => reportError(err,"Error getting Tiles."))
-    .then(tileResponse => getAnnotations(plan)                   .catch(err => reportError(err,"Error getting Annotations."))
-    .then(annotations  => sendTilesToServer(plan.geometry,tileResponse,annotations) .catch(err => reportError(err,"Error contacting server."))
-    .then(response     => getResponseBlob(response)              .catch(err => reportError(err,"Error reading response from server."))
-    .then(responseBlob => readResponseBlob(responseBlob)         .catch(err => reportError(err,"Error reading response from server."))
+  getCurrentlyViewedPlan()                                       .catch(e => reportError(e,"Error getting Plan."))
+    .then(plan         => getTileDataFromPlan(plan)              .catch(e => reportError(e,"Error getting Tiles."))
+    .then(tileResponse => getAnnotations(plan)                   .catch(e => reportError(e,"Error getting Annotations."))
+    .then(annotations  => sendTilesToServer(plan.geometry,tileResponse,annotations) .catch(e => reportError(e,"Error contacting server."))
+    .then(response     => getResponseBlob(response)              .catch(e => reportError(e,"Error reading response from server."))
+    .then(responseBlob => readResponseBlob(responseBlob)         .catch(e => reportError(e,"Error reading response from server."))
     .then(reader       => generatePDF(plan, reader, annotations)
   ))))))
 };
@@ -70,7 +70,7 @@ function sendTilesToServer(planGeo,tileResponse, annotations){
     annotations: annotations
   };
   JSON.stringify(body);
-  return fetch("https://dronedeploy-pdf-generator.herokuapp.com/", {
+  return fetch("https://www.dronedeploy.com/app2/dashboard/", {
     method: "POST",
     body: JSON.stringify(body)
   });
@@ -135,7 +135,5 @@ function generatePDF(plan, reader, annotations){
 
 }
 
-dronedeployApiReady();
-
-
- 
+// event listener
+ddApiMounted();
